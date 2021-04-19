fcba14101f69ae2581ce85ba535f9425bb6d77d2
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-19 11:16:32 -0500

    commit tree 7076930ee1c07a125152e167a9d341d93a692ae5

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+            <label htmlFor="username">Username</label>
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </>
+  </Router>,
+  document.getElementById("root")
 );
